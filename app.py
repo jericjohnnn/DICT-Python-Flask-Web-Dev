f4d5f20260f6f6c1e9860f723ef813b1e9c052b1
Jeric John <jericjohnaligato@gmail.com>
2024-04-05 00:01:36 +0800

    fixed

--- app.py
+++ app.py
@@ -2,7 +2,10 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-
-
 def home():
-    return "Hello, World!"
+    return "<h1>Hello, World!<h1>"
+@app.route('/animal')
+def animal():
+    return """
+    <h1>Fun animal facts!</h1>
+    <p>paragraph</p>"""
